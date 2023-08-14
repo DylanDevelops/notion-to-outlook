a91ab6778cb36ee349ff89ffc747166f90914732
Dylan Ravel <dylanm.ravel@gmail.com>
2023-08-13 19:34:25 -0600

    got the first working version of this working

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client } = require('@notionhq/client');
+require('dotenv').config();
+
+const notion = new Client({
+    auth: process.env.NOTION_INTEGRATION_TOKEN,
+});
+
+async function fetchData() {
+    try {
+        const response = await notion.databases.query({
+            database_id: process.env.NOTION_DATABASE_ID,
+        });
+
+        const data = response.results;
+
+        console.log("Retrieved data:");
+        data.forEach(item => {
+            console.log(item.properties);
+        });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+fetchData();
