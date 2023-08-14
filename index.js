5e54210661d93a121aacf04d2ede8b8397d93e5e
Dylan Ravel <dylanm.ravel@gmail.com>
2023-08-13 22:52:15 -0600

    displaying variables to console

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const notion = new Client({
     auth: process.env.NOTION_INTEGRATION_TOKEN,
 });
 
+let assignmentName;
+let assignmentDeadline;
+let assignmentType;
+let assignmentProgress;
+
 async function fetchData() {
     try {
         const response = await notion.databases.query({
@@ -19,7 +24,43 @@ async function fetchData() {
 
         console.log("Retrieved data:");
         data.forEach(item => {
-            console.log(item.properties);
+            // Access and log the "name" property
+            if (item.properties.name && item.properties.name.title && item.properties.name.title[0]) {
+                assignmentName = item.properties.name.title[0].plain_text;
+            } else {
+                console.error("Item has no 'name' property.");
+            }
+
+            // Access and log the "deadline" property
+            if (item.properties.deadline && item.properties.deadline.date && item.properties.deadline.date.start) {
+                assignmentDeadline = item.properties.deadline.date.start;
+            } else {
+                console.error("Item has no 'deadline' property.");
+            }
+
+            // Access and log the "type" property
+            if (item.properties.type && item.properties.type.select && item.properties.type.select.name) {
+                assignmentType = item.properties.type.select.name;
+            } else {
+                console.error("Item has no 'type' property.");
+            }
+
+            // Access and log the "progress" property
+            if (item.properties.progress && item.properties.progress.status && item.properties.progress.status.name) {
+                assignmentProgress = item.properties.progress.status.name;
+            } else {
+                console.error("Item has no 'progress' property.");
+            }
+
+            console.log(
+                "\n",
+                "Assignment Name: " + assignmentName + "\n",
+                "Assignment Deadline: " + assignmentDeadline + "\n",
+                "Assignment Type: " + assignmentType + "\n",
+                "Assignment Progress: " + assignmentProgress
+            );
+
+            console.log(""); // Print an empty line for separation
         });
     } catch (error) {
         console.error("Error fetching data:", error);
